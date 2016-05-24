fb6988bc9b7f6de43a6b4cb899effbb221cd0b0e
Ivan Bodrov <ibodrov@gmail.com>
2016-05-24 17:11:04 -0400

    simple persistence benchmark

--- benchmark/src/main/java/jet/bpm/benchmark/AbstractBenchmarkState.java
+++ benchmark/src/main/java/jet/bpm/benchmark/AbstractBenchmarkState.java
@@ -1,26 +1,98 @@
 package jet.bpm.benchmark;
 
+import com.google.common.io.Files;
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import jet.bpm.engine.DefaultEngine;
 import jet.bpm.engine.ProcessDefinitionProvider;
 import jet.bpm.engine.api.Engine;
 import jet.bpm.engine.api.ExecutionException;
+import jet.bpm.engine.event.EventPersistenceManager;
+import jet.bpm.engine.event.EventPersistenceManagerImpl;
 import jet.bpm.engine.event.InMemEventStorage;
+import jet.bpm.engine.leveldb.Configuration;
+import jet.bpm.engine.leveldb.KryoSerializer;
+import jet.bpm.engine.leveldb.LevelDbEventStorage;
+import jet.bpm.engine.leveldb.Serializer;
+import jet.bpm.engine.lock.LockManager;
+import jet.bpm.engine.lock.StripedLockManagerImpl;
+import jet.bpm.engine.mapdb.MapDbPersistenceManager;
 import jet.bpm.engine.model.ProcessDefinition;
+import jet.bpm.engine.persistence.MapPersistenceManager;
+import jet.bpm.engine.persistence.PersistenceManager;
 import jet.bpm.engine.task.ServiceTaskRegistry;
+import org.iq80.leveldb.DBFactory;
+import org.iq80.leveldb.impl.Iq80DBFactory;
+import org.openjdk.jmh.annotations.TearDown;
 
 public abstract class AbstractBenchmarkState {
     
     private final Engine engine;
     private final ServiceTaskRegistry serviceTaskRegistry;
+    private LevelDbEventStorage levelDbEventStorage;
+    private MapDbPersistenceManager mapDbPersistenceManager;
     
     public AbstractBenchmarkState(ProcessDefinition def) {
+        this(true, def);
+    }
+    
+    public AbstractBenchmarkState(boolean inMem, ProcessDefinition def) {
         this.serviceTaskRegistry = new DummyServiceTaskRegistry();
         
         DummyProcessDefinitionProvider defs = new DummyProcessDefinitionProvider();
         defs.publish(def.getId(), def);
-        this.engine = new DefaultEngine(defs, serviceTaskRegistry, new InMemEventStorage());
+        
+        LockManager lockManager = new StripedLockManagerImpl(65536);
+        EventPersistenceManager eventPersistenceManager;
+        PersistenceManager persistenceManager;
+        
+        if (inMem) {
+            eventPersistenceManager = new EventPersistenceManagerImpl(new InMemEventStorage());
+            persistenceManager = new MapPersistenceManager(new ConcurrentHashMap<>());
+        } else {
+            String baseDir;
+            try {
+                File f = Files.createTempDir();
+                f.mkdirs();
+                baseDir = f.getAbsolutePath();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            
+            Configuration cfg = new Configuration();
+            cfg.setEventPath(baseDir + "/events");
+            cfg.setExecutionPath(baseDir + "/executions");
+            cfg.setExpiredEventIndexPath(baseDir + "/exprired");
+            cfg.setBusinessKeyEventIndexPath(baseDir + "/bki");
+            
+            DBFactory dbf = new Iq80DBFactory();
+            
+            Serializer serializer = new KryoSerializer();
+            
+            levelDbEventStorage = new LevelDbEventStorage(cfg, dbf, serializer);
+            levelDbEventStorage.init();
+            eventPersistenceManager = new EventPersistenceManagerImpl(levelDbEventStorage);
+            
+            mapDbPersistenceManager = new MapDbPersistenceManager();
+            mapDbPersistenceManager.setBaseDir(baseDir + "/executions");
+            mapDbPersistenceManager.start();
+            persistenceManager = mapDbPersistenceManager;
+        }
+        
+        this.engine = new DefaultEngine(defs, serviceTaskRegistry, eventPersistenceManager, persistenceManager, lockManager);
+    }
+    
+    @TearDown
+    public void close() {
+        if (levelDbEventStorage != null) {
+            levelDbEventStorage.close();
+        }
+        
+        if (mapDbPersistenceManager != null) {
+            mapDbPersistenceManager.stop();
+        }
     }
 
     public final Engine getEngine() {
--- benchmark/src/main/java/jet/bpm/benchmark/PersistenceBenchmark.java
+++ benchmark/src/main/java/jet/bpm/benchmark/PersistenceBenchmark.java
@@ -1,86 +1,47 @@
 package jet.bpm.benchmark;
 
 import java.util.Arrays;
-import jet.bpm.engine.api.ExecutionContext;
+import java.util.UUID;
 import jet.bpm.engine.api.ExecutionException;
-import jet.bpm.engine.api.JavaDelegate;
 import jet.bpm.engine.model.EndEvent;
-import jet.bpm.engine.model.ExpressionType;
+import jet.bpm.engine.model.EventBasedGateway;
+import jet.bpm.engine.model.IntermediateCatchEvent;
 import jet.bpm.engine.model.ProcessDefinition;
 import jet.bpm.engine.model.SequenceFlow;
-import jet.bpm.engine.model.ServiceTask;
 import jet.bpm.engine.model.StartEvent;
-import jet.bpm.engine.task.ServiceTaskResolver;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
 
 /**
- * Linear process with 10 delegate tasks. The delegate task is an instance of
- * {@link JavaDelegate} interface. It is called by resolving its name with
- * {@link ServiceTaskResolver}.
+ * Simple benchmark of the process suspending/resuming.
  */
-public class Linear10DelegateBenchmark {
+public class PersistenceBenchmark {
 
     @State(Scope.Benchmark)
     public static class BenchmarkState extends AbstractBenchmarkState {
 
         public BenchmarkState() {
-            super(new ProcessDefinition("test", Arrays.asList(
+            super(false, new ProcessDefinition("test", Arrays.asList(
                     new StartEvent("start"),
-                    new SequenceFlow("f1", "start", "t1"),
-                    new ServiceTask("t1", ExpressionType.DELEGATE, "${t}"),
-                    new SequenceFlow("f2", "t1", "t2"),
-                    new ServiceTask("t2", ExpressionType.DELEGATE, "${t}"),
-                    new SequenceFlow("f3", "t2", "t3"),
-                    new ServiceTask("t3", ExpressionType.DELEGATE, "${t}"),
-                    new SequenceFlow("f4", "t3", "t4"),
-                    new ServiceTask("t4", ExpressionType.DELEGATE, "${t}"),
-                    new SequenceFlow("f5", "t4", "t5"),
-                    new ServiceTask("t5", ExpressionType.DELEGATE, "${t}"),
-                    new SequenceFlow("f6", "t5", "t6"),
-                    new ServiceTask("t6", ExpressionType.DELEGATE, "${t}"),
-                    new SequenceFlow("f7", "t6", "t7"),
-                    new ServiceTask("t7", ExpressionType.DELEGATE, "${t}"),
-                    new SequenceFlow("f8", "t7", "t8"),
-                    new ServiceTask("t8", ExpressionType.DELEGATE, "${t}"),
-                    new SequenceFlow("f9", "t8", "t9"),
-                    new ServiceTask("t9", ExpressionType.DELEGATE, "${t}"),
-                    new SequenceFlow("f10", "t9", "t10"),
-                    new ServiceTask("t10", ExpressionType.DELEGATE, "${t}"),
-                    new SequenceFlow("f11", "t10", "end"),
+                    new SequenceFlow("f1", "start", "gw"),
+                    new EventBasedGateway("gw"),
+                    new SequenceFlow("f2", "gw", "ev1"),
+                    new IntermediateCatchEvent("ev1", "msg"),
+                    new SequenceFlow("f3", "ev1", "end"),
                     new EndEvent("end"))));
-            
-            getServiceTaskRegistry().register("t", new JavaDelegate() {
-                @Override
-                public void execute(ExecutionContext ctx) throws Exception {
-                    // NOOP
-                }
-            });
         }
     }
 
     @Benchmark
     public void test(BenchmarkState state) {
         try {
-            state.getEngine().start("a", "test", null);
+            String k = UUID.randomUUID().toString();
+            state.getEngine().start(k, "test", null);
+            state.getEngine().resume(k, "msg", null);
         } catch (ExecutionException e) {
             e.printStackTrace();
             System.exit(1);
         }
     }
-    
-    /*
-    public static void main(String[] args) throws RunnerException {
-        Options opt = new OptionsBuilder()
-                .include(Linear10DelegateBenchmark.class.getSimpleName())
-                .forks(0)
-                .warmupIterations(5)
-                .measurementIterations(5)
-                .threads(4)
-                .build();
-
-        new Runner(opt).run();
-    }
-    */
 }
