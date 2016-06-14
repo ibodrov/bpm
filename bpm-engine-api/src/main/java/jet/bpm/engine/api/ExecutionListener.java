fec571a238cfd868c3bcd43056ba20b2c8f97b08
Ivan Bodrov <ibodrov@gmail.com>
2016-06-14 13:53:20 -0400

    javadoc

--- bpm-engine-api/src/main/java/jet/bpm/engine/api/ExecutionListener.java
+++ bpm-engine-api/src/main/java/jet/bpm/engine/api/ExecutionListener.java
@@ -1,5 +1,10 @@
 package jet.bpm.engine.api;
 
+/**
+ * Execution listener. Can be declared for sequence flows in the process
+ * definition. Fires on the flow activation, before the execution moves to the
+ * next element of the process.
+ */
 public interface ExecutionListener {
 
     /**
